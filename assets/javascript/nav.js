d47b187e7e3dffce3e474e1fe7dbb92f9ce8d20e
eedelosreyes2 <eedelosreyes2@dons.usfca.edu>
2020-06-07 18:27:23 -0700

    menu animation

--- assets/javascript/nav.js
+++ assets/javascript/nav.js
@@ -1,15 +1,17 @@
-var opened = false;
+let opened = false;
 
 function toggleNav() {  
   if (!opened) {
     document.getElementById("mySidenav").style.width = "170px";
     document.getElementById("main").style.marginRight = "150px";
     document.getElementById("main").style.marginLeft = "-150px";
+    document.getElementById("menu").classList.add("open");
     opened = true;
   } else {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginRight= "0";
     document.getElementById("main").style.marginLeft = "0";
+    document.getElementById("menu").classList.remove("open");
     opened = false
   }
 }
